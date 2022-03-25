ff2dbeb34d6dd932406715f380a5192b93520481
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-25 23:25:24 +0300

    register location

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Image,Category
+from .models import Image,Category,Location
 admin.site.register(Category)
 admin.site.register(Image)
+admin.site.register(Location)
