c6196d5dfb50bc97d7a685c148f41a7a49c59561
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-25 15:40:57 +0300

    add url routes

--- main/views.py
+++ main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def gallery(request):
     return render(request,'main/gallery.html')
+
+def viewPhoto(request):
+    return render(request,'main/photo.html')
+
+
+
+def addPhoto(request):
+    return render(request,'main/add.html')    
