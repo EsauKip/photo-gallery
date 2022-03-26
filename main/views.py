688abd833b50bef0988abccc39a5e1e811c9e94a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-26 09:17:31 +0300

    display images description

--- main/views.py
+++ main/views.py
@@ -7,8 +7,9 @@ def gallery(request):
     context={'categories':categories,'images':images}
     return render(request,'main/gallery.html',context)
 
-def viewPhoto(request):
-    return render(request,'main/photo.html')
+def viewPhoto(request,pk):
+    image=Image.objects.get(id=pk)
+    return render(request,'main/photo.html',{'image':image})
 
 
 
