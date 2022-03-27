4161cff0ea8241565369c69dbee7997490ca9ff9
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-27 21:35:52 +0300

    run test instance

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from main.models import Category
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.travel= Category(category_name='Travel')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.travel,Category))   
