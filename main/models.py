2f28bc65d450ea6df7a617184648b0c8cde2a66d
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-27 09:37:55 +0300

    add class method for location, save method

--- main/models.py
+++ main/models.py
@@ -4,11 +4,17 @@ from django.db import models
 
 # Create your models here.
 class  Category(models.Model):
-    name = models.CharField(max_length=100,null=False,blank=False)
+    category_name = models.CharField(max_length=100,null=False,blank=False)
 
 
     def __str__(self):
-        return self.name
+        return self.category_name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()    
 
 
 
@@ -29,3 +35,14 @@ class Location(models.Model):
     
     def __str__(self):
         return self.name 
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def get_locations(cls):
+        location=Location.objects.all()
+
+        return location    
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from .models import Category,Image
 # Create your views here.
 def gallery(request):
@@ -15,5 +16,24 @@ def viewPhoto(request,pk):
 
 def addPhoto(request):
     categories=Category.objects.all()
+    if request.method == 'POST':
+        data =request.POST
+        image=request.FILES.get('image')
+        if data['category'] !='none':
+            category=Category.objects.get(id=data['category'])
+        elif data['category_new']!='':
+            category,created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category=None 
+            image=Image.objects.create(
+            category=category,
+            description=data['description'],
+            image=image,
+        ) 
+
+        return redirect('gallery')       
+                  
+      
+
     context={'categories':categories}
     return render(request,'main/add.html',context)    
