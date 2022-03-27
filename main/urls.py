05f2e9e7921ecd7ac40c2c8f32cef96ba482a307
EsauKip <esau.kiprono@student.moringaschool.com>
2022-03-27 21:28:30 +0300

    modify templates

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -8,3 +10,5 @@ urlpatterns = [
     path('add/', views.addPhoto, name='add'),
     path('location/<str:location_name>/',views.image_location,name ='location'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
